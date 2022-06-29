69e593606cf0374aa84a80a715ad6e10f5d6b257
michael <forgetscode@gmail.com>
2022-06-28 20:29:52 -0700

    initialize chat function

--- programs/sms/src/lib.rs
+++ programs/sms/src/lib.rs
@@ -5,11 +5,67 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod sms {
     use super::*;
+    pub fn initialize_chat(ctx: Context<InitializeChat>) -> Result<()> {
+        let chat = &mut ctx.accounts.chat;
+        chat.initializer = ctx.accounts.initializer.key();
+        chat.recepient = ctx.accounts.receiver.key();
+        Ok(())
+    }
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize_message(ctx: Context<InitializeMessage>) -> Result<()> {
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitializeChat<'info>  {
+    #[account(
+        init,
+        payer = initializer,
+        space = 8 + 32 + 32,
+    )]
+    pub chat: Account<'info, Chat>,
+    #[account(mut)]
+    pub initializer: Signer<'info>,
+    pub receiver: SystemAccount<'info>,
+    pub system_program: Program<'info, System>
+}
+
+#[derive(Accounts)]
+#[instruction(text:String)]
+pub struct InitializeMessage<'info>  {
+    #[account(
+        init,
+        payer = initializer,
+        space = Message::space(&text),
+    )]
+    pub message: Account<'info, Message>,
+    #[account(mut)]
+    pub initializer: Signer<'info>,
+    pub receiver: SystemAccount<'info>,
+    pub system_program: Program<'info, System>
+}
+
+#[account]
+pub struct Chat {        //8
+    initializer: Pubkey, //32
+    recepient: Pubkey,   //32
+}
+
+impl Message {
+    fn space(text: &str) -> usize {
+        // discriminator
+        8 +
+        // String
+        4 + text.len() +
+        // u8
+        1
+    }
+}
+
+#[account]
+pub struct Message {
+    message: String,    //255
+    mark: u8,           //1
+}
--- tests/sms.ts
+++ tests/sms.ts
@@ -1,5 +1,6 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
+import { assert } from "chai";
 import { Sms } from "../target/types/sms";
 
 describe("sms", () => {
@@ -8,9 +9,50 @@ describe("sms", () => {
 
   const program = anchor.workspace.Sms as Program<Sms>;
 
+
+  const receiver = anchor.web3.Keypair.generate();
+
+  const initializer = anchor.web3.Keypair.generate();
+
+  const chat = anchor.web3.Keypair.generate();
+  
+
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+
+    //airdrop lamports to accounts
+    await program.provider.connection.confirmTransaction(
+      await program.provider.connection.requestAirdrop(initializer.publicKey, 10000000000),
+      "confirmed"
+    );
+
+
+    //if you don't have lamports you will get error 0x1
+    let balancebefore = await program.provider.connection.getBalance(initializer.publicKey);
+    console.log("initializer balance: ", balancebefore* (10**-9));
+
+    const tx = await program.methods.initializeChat()
+    .accounts(
+      {
+        chat: chat.publicKey,
+        initializer: initializer.publicKey,
+        receiver: receiver.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+    ).signers([chat, initializer]).rpc();
+
+    //program accounts, gets all accounts owned by our program in an array
+    const programAccounts = await program.provider.connection.getProgramAccounts(program.programId);
+    //console.log("chat account", programAccounts);
+
+    //toBase58 - Binary -> readable
+    //get owner of our chat account which is the programID
+    //console.log(programAccounts[0].account.owner.toBase58());
+
+    //get account data
+    let chatAccount = await program.account.chat.fetch(chat.publicKey);
+    console.log("chat account", chatAccount);
+
     console.log("Your transaction signature", tx);
   });
 });
